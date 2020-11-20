a9e3f0a9764f74fb4e3e045508512365527ab162
jtate1166 <jtate1166@gmail.com>
2020-11-20 15:51:48 -0500

    makes nav bar responsive to different screen sizes

---
+++ app.js
@@ -0,0 +1,8 @@
+const showMenu = () => {
+    let menu = document.getElementById('links-mobile');
+    if (menu.style.display === 'none') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+}
